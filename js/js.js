1e17af3e1fd18bebed72ab6d07f3f04d1c26eca3
Artem Svirskyi <asv@qapint.com>
2014-09-10 22:47:26 +0300

    working example fix

--- js/js.js
+++ js/js.js
@@ -1,20 +1,36 @@
-var flag = false;
+var flag = false,
+	event;
 
 toggle.addEventListener("click", function(){
 	front.classList.toggle('animate');
 });
 
 front.addEventListener("webkitTransitionEnd", function(event){
-	// article.classList.toggle('animate');
-	setTimeout(function(){
-		if(flag){
+	// webkitRequestAnimationFrame(function(){
+		// alert(1);
+	if(event.propertyName === '-webkit-transform'){
+		article.classList.toggle('animate');
+		// alert(1);
+	}
+	// });
+
+
+	// alert(webkitRequestAnimationFrame);
+	// setTimeout(function(){
+	// alert(event.propertyName);
+/*		if(flag){
 			front.style.left = '0px';
 		}else{
 			front.style.left = '100px';
 		}
 		flag = !flag;
-	}, 100);
+*/	// front.dispatchEvent(new CustomEvent('blabla', { bubbles: true, cancelable: false }));
+	// }, 100);
+	// console.log('end');
 }, true);
+
+
+// alert(window.hasOwnProperty('transitionend'));
 
 /*setInterval(function(){
 	front.classList.toggle('animate');
