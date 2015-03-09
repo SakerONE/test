c37e0f2ede1648033f0be8f4bb6117026e5c15c7
Artem Svirskyi <asv@qapint.com>
2015-03-09 21:36:08 +0200

    edit

--- js/js.js
+++ js/js.js
@@ -1,37 +1,17 @@
-var flag = false,
-	event;
+function Parent(){
+	this.name = "Adam";
+}
 
-toggle.addEventListener("click", function(){
-	front.classList.toggle('animate');
-});
+Parent.prototype.say = function(){
+	console.log(this.name);
+};
 
-front.addEventListener("webkitTransitionEnd", function(event){
-	// webkitRequestAnimationFrame(function(){
-		// alert(1);
-	if(event.propertyName === '-webkit-transform'){
-		article.classList.toggle('animate');
-		// alert(1);
-	}
-	// });
+var parent = new Parent();
 
+function Child(){
 
-	// alert(webkitRequestAnimationFrame);
-	// setTimeout(function(){
-	// alert(event.propertyName);
-/*		if(flag){
-			front.style.left = '0px';
-		}else{
-			front.style.left = '100px';
-		}
-		flag = !flag;
-*/	// front.dispatchEvent(new CustomEvent('blabla', { bubbles: true, cancelable: false }));
-	// }, 100);
-	// console.log('end');
-}, true);
+}
 
+Child.prototype = Object.create(Parent.prototype);
 
-// alert(window.hasOwnProperty('transitionend'));
-
-/*setInterval(function(){
-	front.classList.toggle('animate');
-}, 2000);*/
+console.log(new Child());
